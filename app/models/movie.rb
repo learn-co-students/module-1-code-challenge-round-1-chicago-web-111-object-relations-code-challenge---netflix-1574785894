a4ab4df08b310cc23e544376fd0d64e0e151b816
kHuynhGitHub <huynh.h.kiimberly@gmail.com>
2019-11-26 12:14:19 -0600

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,24 @@ class Movie
     @@all
   end
 
+  def average_rating
+    ratings = self.reviews.map {|review|review.rating}
+    return (ratings.sum / ratings.size)
+  end
+
+  #Movie.highest_rated
+  #returns the Movie instance with the highest average rating.
+  #Do last cause unsure
+  def self.highest_rated
+    return self.average_rating.max {|r1, r2| r1.average_rating.max <=> r2.average_rating.max}
+  end
+
+  def reviews
+    Review.all.select {|review| review.movie == self}
+  end
+
+  def reviewers
+    reviewers = self.reviews.map {|review| review.viewer}
+    reviewers.uniq
+  end
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,20 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def reviewed_movie?(movie)
+    reviews.include?(movie) ? true : false
+  end
+
+  def rate_movie(movie, rating)
+    Review.new(self, movie, rating)
+  end
+
+  def reviews
+    Review.all.select {|review| review.viewer == self}
+  end
+
+  def reviewed_movies
+    reviews.select {|movie| movie.viewer == self}
+  end
 end
