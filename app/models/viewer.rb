60178b70db4f2145d684511616a365a7d17dc1a6
hfarnsworth <havilahfarnsworth@gmail.com>
2019-11-26 12:07:47 -0600

    done

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -12,4 +12,31 @@ class Viewer
     @@all
   end
   
+  def reviews
+    Review.all.select do
+      |review| review.viewer == self
+    end
+  end
+
+  def reviewed_movies
+    self.reviews.map do 
+      |review| review.movie
+    end
+  end
+
+  def viewed_movie?(movie)
+    self.reviews.any? { |review| review.movie == movie }
+  end
+  
+  def rate_movie(movie,rating)
+    if self.viewed_movie?(movie)
+      #checks if viewed, and if such updates the rating value
+      already_reviewed = self.reviews.find do
+        |review| review.movie == movie
+      end
+      already_reviewed.rating = rating
+    else
+      Review.new(self,movie,rating)
+    end
+  end
 end
