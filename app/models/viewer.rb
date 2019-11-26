b14c8b62fd2a68611a6f465faaa8890754a42766
github username <github email address>
2019-11-26 12:08:52 -0600

    done

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,27 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def reviews
+    Review.all.select{|person| person.viewer == self}
+  end
+
+  def reviewed_movies
+    Review.all.select{|mov| mov.movies == self}
+  end
+
+  # def reviewed_movie?(movie)
+  #   if #Viewer has reviewed Movie
+  #     true
+  #   else
+  #     false
+  #   end
+
+  #   def rate_movie(movie,rating)
+  #     if reviewed_movie?
+  #       revise Review(self, movie, rating)
+  #     else
+  #       Review.new(self, movie, rating)
+  #     end
+  #   end
 end
