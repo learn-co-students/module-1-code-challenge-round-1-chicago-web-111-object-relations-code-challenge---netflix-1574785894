38cd0ad1285adebe9a3e6cfd67b08ee8a0fa4e7e
dut-a <d.dut@live.com>
2019-11-26 12:13:05 -0600

    Done with code Challenge 1

--- tools/console.rb
+++ tools/console.rb
@@ -10,6 +10,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+# Movies
+movie1 = Movie.new("Despicable Me")
+movie2 = Movie.new("Hunting")
+
+# Reviewers
+viewer1 = Viewer.new("Dut")
+viewer2 = Viewer.new("Daniel")
+
+# Reviews
+review1 = Review.new(viewer1, movie1, 5)
+review2 = Review.new(viewer2, movie2, 3)
+review2 = Review.new(viewer2, movie1, 2)
+
+
 
 
 
