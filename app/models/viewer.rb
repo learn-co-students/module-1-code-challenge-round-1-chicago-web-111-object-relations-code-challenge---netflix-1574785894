5df1a629338ffc63eb95445accc328178dc655a9
github username <github email address>
2019-11-26 12:15:14 -0600

    done

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -5,11 +5,36 @@ class Viewer
 
   def initialize(username)
     @username = username
-    self.class.all << self
+    @@all << self
   end
 
   def self.all
     @@all
   end
-  
+
+  def reviews
+    Review.all.select {|r| r.viewer == self}
+  end
+
+  def reviewed_movies
+    self.reviews.map {|m| m.movie}
+  end
+
+  def reviewed_movie?(movie)
+    self.reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating)
+    hash = Hash.new
+    Review.all.each do |x|
+      hash[x.movie] = self.reviewed_movie?(movie)
+    end
+
+    # hash.each do |k,v|
+    #   if v == true && k = Movie.reviews
+    #     Review.k
+    #   end
+
+    # binding.pry
+  end
 end
