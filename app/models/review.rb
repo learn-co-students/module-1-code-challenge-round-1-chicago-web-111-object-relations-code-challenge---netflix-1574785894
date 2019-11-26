e2dd084813066ff3e7b1d879c90ea2bc304301c8
Andrea <andreabarr87@gmail.com>
2019-11-26 12:08:31 -0600

    doneish

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,21 @@
-class Review
+class Review #is join class of movie and viewer
+    attr_reader :viewer, :movie
+    attr_writer 
+    attr_accessor :rating
+
+    @@all = []
+
+    def initialize(viewer, movie, rating) 
+        @viewer = viewer
+        @movie = movie
+        @rating = rating #is integer
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 
 
 end
