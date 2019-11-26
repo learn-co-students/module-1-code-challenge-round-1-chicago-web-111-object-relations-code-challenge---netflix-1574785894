71478ba749d71cb88ec58433593adcb981af2616
github username <github email address>
2019-11-26 12:07:19 -0600

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,58 @@ class Movie
     @@all
   end
 
+  ##returns an array of all the Review instances for the Movie
+  def reviews
+    Review.all.select{|review_value| review_value.movie == self.title }
+  end
+
+  ##returns an arry of all of the Viewer instances that reviewed the Movie
+  def reviewers
+    self.reviews.map {|review_value| review_value.viewer}
+  end
+
+  ##### COME AND REFACTORreturns the average of all ratings for the Movie instance to average ratings, add all ratings together and divide by the total number of ratings
+  def average_rating
+    list = reviews #list of reviews that exist for this (self) movie instance
+    ratings = reviews.collect {|m_instance| m_instance.rating}
+    ratings_len = ratings.length 
+    ratings_sum = ratings.inject(0) {|sum, rating_value| sum += rating_value}
+    average = ratings_sum.to_f / ratings_len.to_f
+    # binding.pry
+  end
+
+  def average_method
+    list = Review.all.select{|review_value| review_value.movie == self} 
+    ratings = list.collect {|m_instance| m_instance.rating}.length
+    ratings_len = ratings.length 
+    ratings_sum = ratings.inject(0) {|sum, rating_value| sum += rating_value}
+    average = ratings_sum.to_f / ratings_len.to_f
+  end
+  #CLASS METHOD. returns the Movie instance with the highest average rating.
+  def self.highest_rated
+    all_movies = Review.all.map{|value| value.movie} #list of movies reviewed
+    
+    all_avg_ratings = all_movies.collect do |movie| 
+    # binding.pry
+    list = Review.all.select{|review_value| review_value.movie == movie}
+    ratings = list.collect {|m_instance| m_instance.rating}
+    ratings_len = ratings.length 
+    ratings_sum = ratings.inject(0) {|sum, rating_value| sum += rating_value}
+    average = ratings_sum.to_f / ratings_len.to_f
+    end #array of average ratings
+    
+    high = 0
+    name = nil 
+    i = 0
+
+    while i < all_avg_ratings.length do 
+      if high < all_avg_ratings[i]
+        high = all_avg_ratings[i]
+        name = all_movies[i]
+      end
+      i += 1
+      # binding.pry
+    end
+    return name
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,10 +10,38 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+lordsawe = Viewer.new("Lordsawe")
+swizz = Viewer.new("SwizzDefeatz")
+kev = Viewer.new("K-Flo")
+
+beautiful = Movie.new("It's Such a Beautiful Day")
+interstellar = Movie.new("Interstellar")
+martian = Movie.new("The Martian")
+star_wars_6 = Movie.new("Star Wars: Revenge of the Sith")
+
+review_1 = Review.new(lordsawe.username, beautiful.title, 5)
+review_2 = Review.new(swizz.username, beautiful.title, 3)
+review_3 = Review.new(kev.username, beautiful.title, 1)
+
+review_4 = Review.new(lordsawe.username, interstellar.title, 4)
+review_5 = Review.new(swizz.username, martian.title, 4)
+review_6 = Review.new(kev.username, star_wars_6.title, 4)
+
+# p lordsawe.reviews
+# p swizz.reviewed_movies
+# p kev.reviewed_movie?(beautiful.title) # should be true
+# p kev.reviewed_movie?(interstellar.title) #shoudl be false
+# p lordsawe.rate_movie(interstellar.title, 5)
+# p kev.rate_movie(interstellar.title, 5)
+# p beautiful.reviews
+# p beautiful.reviewers
+p beautiful.average_rating #REFACTOR METHOD
+p Movie.highest_rated #works but needs SERIOUS REFACTORING and REORGANIZING i.e. it looks like noodles
+
 
 
 
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
-binding.pry
+# binding.pry
 0
